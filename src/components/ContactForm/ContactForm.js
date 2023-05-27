a82184d558e68560b155a246a6cb24f20ace3143
Alex Silviu Scripcariuc <alex.silviu.scripcariuc@gmail.com>
2023-05-27 09:44:21 +0300

    form test

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -7,7 +7,7 @@ export default function ContactForm() {
     return (
     <div className={`${styles.contactForm}`}>
 
-        <form  name="Alex Silviu Contact Form" className={`${styles.form} flex flex-col gap-8`} netlify data-netlify="true">
+        <form  name="Alex Silviu Contact Form" className={`${styles.form} flex flex-col gap-8`} method="post" data-netlify="true" data-netlify-honeypot="bot-field">
             <div className="flex flex-col gap-4">
                 <label htmlFor="name">Name</label>
                 <input type="text" name="name" id="name" placeholder="Your name" required />
