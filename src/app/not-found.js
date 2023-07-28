74307fa1b9f41b8fb978a977dc22837d78b19fa2
Alex Silviu Scripcariuc <alex.silviu.scripcariuc@gmail.com>
2023-07-28 11:22:40 +0300

    404 error update

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,17 +1,28 @@
 "use client";
-import Link from "next/link";
+import { useState, useEffect } from "react";
 
 export default function NotFound() {
+  const [countdown, setCountdown] = useState(10); // Countdown in seconds
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCountdown((prevCountdown) => prevCountdown - 1);
+    }, 1000);
+
+    if (countdown === 0) {
+      window.location.href = "https://alexsilviu.vercel.app/";
+    }
+
+    return () => clearInterval(timer);
+  }, [countdown]);
 
   return (
-    <div className="flex flex-col items-center justify-center w-full h-screen gap-16">
+    <div className="flex flex-col items-center justify-center w-full h-screen gap-16 ">
       <h1>404</h1>
       <h2>Page not found</h2>
       <h3>
-        How about you go{" "}
-        <Link href="/" legacyBehavior>
-          <a style={{ color: 'var(--gold)' }} >home</a>
-        </Link>
+        Redirecting you home in{" "}
+        <span className="link">{countdown} seconds...</span>
       </h3>
     </div>
   );
